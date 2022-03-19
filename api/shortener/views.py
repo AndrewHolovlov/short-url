bfe876aba841431a3c531c70c3665046f0df6f8e
andrew <a.holovlov@zd.ua>
2022-03-19 13:45:37 +0200

    Changed validator to marshmallow

--- api/shortener/views.py
+++ api/shortener/views.py
@@ -1,34 +1,21 @@
 import datetime
 
-from flask import redirect
-from flask_restful import Resource, reqparse
+from flask import redirect, request
+from flask_restful import Resource
 
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask_apispec.views import MethodResource
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
+from marshmallow.validate import Range
 
 from api.models.shortener import ShortUrl
 from api.database import db
 
 
-def life_span(life_span_int):
-    if isinstance(life_span_int, int):
-        if life_span_int in range(1, 256):
-            return life_span_int
-        else:
-            raise ValueError('life_span must be between 1 and 256')
-    else:
-        raise ValueError('life_span must be integer')
-
-
-post_parser = reqparse.RequestParser()
-post_parser.add_argument('long_url', type=str, help='long_url is required', required=True)
-post_parser.add_argument('life_span', type=life_span, default=90)
-
-
 class CreateShortUrlParamsSchema(Schema):
-    long_url = fields.Str()
-    life_span = fields.Int()
+    long_url = fields.Str(required=True)
+    life_span = fields.Int(default=90, load_default=90,
+                           validate=[Range(min=1, max=256, error="value must be between 1 and 256")])
 
 
 class CreateShortUrlResponseSchema(Schema):
@@ -37,26 +24,33 @@ class CreateShortUrlResponseSchema(Schema):
     expiry_at = fields.DateTime()
 
 
+post_schema = CreateShortUrlParamsSchema()
+
+
 class CreateShortUrl(MethodResource, Resource):
     @doc(description='Create short url', tags=['Short url'])
-    @use_kwargs(CreateShortUrlParamsSchema, location=('json'))
+    @use_kwargs(CreateShortUrlParamsSchema, location=('json'), apply=False)
     @marshal_with(CreateShortUrlResponseSchema,
                   description='Update expiry time if expiry time expired else return short url', code=200)
     @marshal_with(CreateShortUrlResponseSchema, description='Create short url', code=201)
     def post(self, **kwargs):
-        args = post_parser.parse_args()
+        json_data = request.get_json()
+        try:
+            data = post_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 400
 
-        obj = db.session.query(ShortUrl).filter(ShortUrl.long_url == args['long_url']).first()
+        obj = db.session.query(ShortUrl).filter(ShortUrl.long_url == data['long_url']).first()
         if obj:
             if obj.expiry_at > datetime.datetime.now():
                 return obj, 200
             else:
                 obj.created_at = datetime.datetime.now()
-                obj.expiry_at = obj.created_at + datetime.timedelta(days=args['life_span'])
+                obj.expiry_at = obj.created_at + datetime.timedelta(days=data['life_span'])
                 db.session.commit()
                 return obj, 200
         else:
-            obj = ShortUrl(long_url=args['long_url'], life_span=args['life_span'])
+            obj = ShortUrl(long_url=data['long_url'], life_span=data['life_span'])
             db.session.add(obj)
             db.session.commit()
             return obj, 201
